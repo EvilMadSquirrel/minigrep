cf6250ff1933393a56a00d9e65826bccbf027105
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-11-14 15:37:13 +0300

    moved logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
+use minigrep::Config;
 
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len()<3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,10 +15,11 @@ fn main() {
     println!("Searching for: {}", config.query);
     println!("In file: {}", config.file_path);
 
-    let content =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With content:\n {}", content);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error {e}");
+        process::exit(1);
+    }
 }
+
 
 
